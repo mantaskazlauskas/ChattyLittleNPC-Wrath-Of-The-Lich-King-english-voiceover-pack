9ecb2be5a298ae123f79bca15d88987ce005a059
Mantas Kazlauskas <mmantas.kazlauskas@gmail.com>
2025-11-12 23:37:14 +0200

    Refactor ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers to remove Ace3 dependency
    - Deleted README.md and changelog.txt files related to Ace3.
    - Updated Main.lua and VoiceoversList.lua to directly reference the addon from the global namespace instead of using LibStub.
    - Removed embeds.xml file that included Ace3 libraries.
    - Modified generate_voicovers_list_file.py to generate Lua files that reference the addon from the global namespace.

--- ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers/VoiceoversList.lua
+++ ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers/VoiceoversList.lua
@@ -1,5 +1,5 @@
 ---@class ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers
-local ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers = LibStub("AceAddon-3.0"):GetAddon("ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers")
+local ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers = _G["ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers"]
 
 ChattyLittleNpc_Wrath_of_the_Lich_King_voiceovers.Voiceovers = {
     '11153_Comp.ogg',
